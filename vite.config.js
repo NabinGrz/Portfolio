2f424dde880002fc8df62cb35eb841ce2407e52b
“Nabin <nobig22@gmail.com>
2026-02-18 09:59:56 +0545

    feat: Conditionally set Vite base URL to `/Portfolio/` for production and `/` for other environments.

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/Portfolio/',
+  base: mode === 'production' ? '/Portfolio/' : '/',
   plugins: [
     react(),
   ],
